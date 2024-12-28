22d2c4e15291963ccbacded37a18c8d8d504868a
arav <mearavindkumar@gmail.com>
2024-12-28 21:13:54 +0530

    dec28

--- src/lab/daily/Dec28.java
+++ src/lab/daily/Dec28.java
@@ -1,5 +1,34 @@
 package lab.daily;
 
+import lab.base.TreeNode;
+
 public class Dec28 {
+
+	public boolean binarySearch(TreeNode root, int target) {
+		if (root == null) {
+			return false;
+		}
+		if (root.val == target) {
+			return true;
+		}
+		return root.val < target ? binarySearch(root.right, target) : binarySearch(root.left, target);
+	}
+
+	public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+		return dfs(root1, root2, target);
+	}
+
+	public boolean dfs(TreeNode root1, TreeNode root2, int target) {
+		if (root1 == null) {
+			return false;
+		}
+		if (binarySearch(root2, target - root1.val)) {
+			return true;
+		}
+
+		return dfs(root1.left, root2, target) || dfs(root1.right, root2, target);
+	}
+
+
 
 }
